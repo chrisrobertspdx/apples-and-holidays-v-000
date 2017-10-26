3e46964653600073befd5f705884452ac81d1183
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-26 20:03:55 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,7 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash[:winter].each{|key,val|
     #holiday_hash[:winter][key].push(supply)
-    this.[key].push(supply)
+    this[key].push(supply)
   }
 end
 
